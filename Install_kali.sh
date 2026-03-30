fe200ada5b1f2169b9773eecd02f971713ac9414
SuperRoot-X64 <anas.sabir.idrissi@gmail.com>
2026-03-30 16:13:55 -0400

    Create Install_kali.sh
    very legit working scirpt

---
+++ Install_kali.sh
@@ -0,0 +1,8 @@
+termux-setup-storage
+pkg update 
+pkg upgrade
+clear
+pkg install wget 
+wget -O install-nethunter-termux https://offs.ec/2MceZWr
+chmod +x install-nethunter-termux
+./install-nethunter-termux
